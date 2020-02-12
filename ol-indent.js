93812c4a9f5db5582ef8c8b1b6491aa2ec70e575
Paul Bredenberg <paulbredenberg@gmail.com>
2020-02-12 16:43:46 -0500

    feat: make unordered list linting work

--- ol-indent.js
+++ ol-indent.js
@@ -1,47 +1,48 @@
 'use strict';
 
-const { addErrorDetailIf, indentFor, orderedListItemMarkerRe } =
-  require('./node_modules/markdownlint/helpers');
+const { addErrorDetailIf, indentFor, orderedListItemMarkerRe, flattenLists } =
+  require('markdownlint-rule-helpers');
 
-const parseList = (params, list, onError) => {
-   if (list.ordered && list.parentsOrdered) {
-      const indent = Number(params.config.indent || 2);
-
-      const startIndented = params.config.start_indented;
-
-      list.items.forEach((item) => {
-         const { lineNumber, line } = item,
-               expectedNesting = list.nesting + (startIndented ? 1 : 0),
-               expectedIndent = expectedNesting * indent,
-               actualIndent = indentFor(item),
-               match = line.match(orderedListItemMarkerRe);
-
-         let range = null,
-             editColumn = 1;
-
-         if (match) {
-            range = [ 1, match[0].length ];
-            editColumn += match[1].length - actualIndent;
-         }
-
-         addErrorDetailIf(onError, lineNumber, expectedIndent, actualIndent, null, null, range, {
-            editColumn,
-            'deleteCount': actualIndent,
-            'insertText': ''.padEnd(expectedIndent),
-         });
-      });
-   }
-};
+const { URL } = require('url');
 
 module.exports = {
    'names': [ 'ol-indent' ],
    'description': 'Ordered list indentation',
    'information': new URL('https://example.com/rules/any-blockquote'),
    'tags': [ 'ol', 'indentation' ],
-   'function': function MD008(params, onError) {
-      params.tokens.forEach((token) => {
-         console.log('token', token); // eslint-disable-line
-         return parseList(params, token, onError);
+   'function': (params, onError) => {
+      const indent = Number(params.config.indent || 2),
+            startIndented = params.config.start_indented;
+
+      flattenLists(params).forEach((list) => {
+         if (list.ordered && list.parentsOrdered) {
+            list.items.forEach((item) => {
+               // The below logic is essentially the same as the rule for checking
+               // indentation in unordered lists. If this rule is eventually integrated
+               // into markdownlint, it would make sense to add the below to a helper
+               // function that both can use.
+               // See: https://github.com/DavidAnson/markdownlint/blob/master/lib/md007.js#L17
+               const { lineNumber, line } = item,
+                     expectedNesting = list.nesting + (startIndented ? 1 : 0),
+                     expectedIndent = expectedNesting * indent,
+                     actualIndent = indentFor(item),
+                     match = line.match(orderedListItemMarkerRe);
+
+               let range = null,
+                   editColumn = 1;
+
+               if (match) {
+                  range = [ 1, match[0].length ];
+                  editColumn += match[1].length - actualIndent;
+               }
+
+               addErrorDetailIf(onError, lineNumber, expectedIndent, actualIndent, null, null, range, {
+                  editColumn,
+                  'deleteCount': actualIndent,
+                  'insertText': ''.padEnd(expectedIndent),
+               });
+            });
+         }
       });
    },
 };
